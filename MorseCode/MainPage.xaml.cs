95d624ffc6ec8b84cc7a7fdbc568308e00e8d20e
cpavelsek <Furry81github>
2021-04-12 16:07:13 -0500

    Initial check-in of module MorseCode

---
+++ MorseCode/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MorseCode
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+
+        private string dots = "";
+        private string dashes = "";
+        private string space = "";
+
+        void Dot_Clicked(System.Object sender, System.EventArgs e)
+        {
+            dots += ".";
+
+            DotsAndDashes.Text = dots;
+            
+        }
+
+
+        void Dash_Clicked(System.Object sender, System.EventArgs e)
+        {
+            dashes += "=";
+
+            DotsAndDashes.Text = dots;
+
+        }
+
+        void Space_Clicked(System.Object sender, System.EventArgs e)
+        {
+            space += " ";
+
+            DotsAndDashes.Text = space;
+
+            if(space.Length == 2)
+            {
+                var words = Morse.MorseCoder(dots + dashes);
+                LettersAndWords.Text = words.ToString();
+            }
+
+        }
+
+
+
+    }
+}
